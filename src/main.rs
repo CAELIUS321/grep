5252ceda25cf471068a686002cf0eaaa17dc004e
CAELIUS321 <1911281217@qq.com>
2023-05-25 16:34:23 +0800

    a simple grep

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use grep::Config;
+use std::{env,process};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config=Config::new(&args).unwrap_or_else(|error|{
+        eprintln!("Problem parsing arguments: {}", error);
+        process::exit(1);
+    });
+
+    if let Err(e)=grep::run(config){
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+}
+
+
